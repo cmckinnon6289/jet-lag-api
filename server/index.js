e16f2b45899d458a435df862b5cd3225f34e4b6d
cmckinnon6289 <cmckinnon@bayviewglen.ca>
2024-02-23 10:15:16 -0500

    maybe this was it

--- server/index.js
+++ server/index.js
@@ -11,7 +11,7 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => console.log(`server started port ${PORT}`))
-app.use(express.json());
+app.use(bodyParser.json());
 
 app.get('/', (req,res) => res.send('hello. get off the API page.'))
 
