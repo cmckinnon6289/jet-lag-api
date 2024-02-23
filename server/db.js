bca27aa604d1ef15bd2ced1265598f15ef06ce5e
cmckinnon6289 <cmckinnon@bayviewglen.ca>
2024-02-23 16:08:06 -0500

    fixed. HOORAY!

--- server/db.js
+++ server/db.js
@@ -1,6 +1,6 @@
-import mongoose from "mongoose";
+const mongoose = require("mongoose")
 
-const connectionString = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@${process.env.DB_CLUSTER}.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
+const connectionString = `mongodb+srv://callum:7nwOqqmVPqVgroM4@jet-lag-knockoff.rnwupgu.mongodb.net/?retryWrites=true&w=majority`;
 
 mongoose.connect(connectionString, {
     useNewUrlParser: true,
--- server/index.js
+++ server/index.js
@@ -1,70 +1,23 @@
-import express from 'express';
-import cors from 'cors';
-import bodyParser from 'body-parser';
-import mongoose from 'mongoose';
-
-require('./db')
-
-const Challenge = require('./models/Challenge')
-const Curse = require('./models/Curse')
+const express = require('express')
+const cors = require('cors')
+const bodyParser = require('body-parser')
+const mongoose = require('mongoose')
 
 const app = express();
-const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => console.log(`server started port ${PORT}`))
 app.use(bodyParser.json());
 app.use(cors());
 
+require('./db')
+const Challenge = require('./models/Challenge')
+const Curse = require('./models/Curse')
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => console.log(`server started port ${PORT}`))
+
+
 app.get('/', (req,res) => res.send('hello. get off the API page.'))
-
-const CARDS = [
-    {
-        tokens: 1,
-        canActive: true,
-        canVeto: true,
-        name: "Your city!",
-        description: "Report 3 safety concerns (potholes, broken traffic lights, etc) to 311. You must report each concern in separate calls. Alternatively, remove 10 pieces of litter from  a park. The litter does not all have to be from the same park."
-    },
-    {
-        challenge: "hi"
-    },
-    {
-        challenge: "hello"
-    },
-    {
-        challenge: ":)"
-    },
-    {
-        challenge: "be happy"
-    },
-    {
-        challenge: "be grateful you have all these wonderful things"
-    },
-    {
-        challenge: "be grateful you have all these wonderful people in your life :)"
-    }
-]
-
-const CURSES = [
-    {
-        tokens: 0,
-        canActive: false,
-        canVeto: false,
-        name: "Blackout",
-        description: "Neither teammate may use a phone to research public transit routes for the remainder of the game. This card cannot be vetoed."
-    },
-    {
-        curse: "hi"
-    },
-    {
-        curse: "hello"
-    },
-    {
-        curse: ":)"
-    }
-]
-
-const deckLen = CARDS.length + CURSES.length;
 
 app.get('/api/assets/curses', async (req, res) => {
     try {
@@ -82,38 +35,4 @@ app.get('/api/assets/challenges', async (req, res) => {
     } catch(err) {
         res.status(500).json({ error: err.message });
     }
-})
-
-app.get('/api/get-card', (req, res) => {
-    let card;
-    let num = Math.round(Math.random()*deckLen);
-    if (num >= CARDS.length) {
-        num -= CARDS.length;
-        card = CURSES[num];
-    } else {
-        card = CARDS[num];
-    }
-    res.send(card ? card : "nothing found");
-})
-
-app.post('/api/new-card', (req, res) => {
-    const newCard = {
-        tokens: req.body.tokens,
-        canActive: req.body.canActive,
-        canVeto: req.body.canVeto,
-        name: req.body.name,
-        description: req.body.description
-    }
-
-    let push;
-
-    if (req.body.type == 1) { 
-        CURSES.push(newCard)
-        push = "curse";
-    }
-    else {
-        CARDS.push(newCard)
-        push = "challenge";
-    };
-    res.send(`for type ${req.body.type}, added a new ${push} card`)
 })
