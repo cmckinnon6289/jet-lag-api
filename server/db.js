dcb18656cf24c37d983f08931e146706fb1c8aaa
cmckinnon6289 <cmckinnon@bayviewglen.ca>
2024-02-23 21:05:00 -0500

    correctly pathed the documents

--- server/db.js
+++ server/db.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose")
 const enviro = require("dotenv")
 
 enviro.config()
-const connectionString = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@${process.env.DB_CLUSTER}.rnwupgu.mongodb.net/?retryWrites=true&w=majority`;
+const connectionString = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@${process.env.DB_CLUSTER}.rnwupgu.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
 
 mongoose.connect(connectionString, {
     useNewUrlParser: true,
